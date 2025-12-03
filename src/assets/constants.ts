4ccd7e1d81c29116680c216e3ecba2ebc5aaa37b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-03 17:24:21 +0530

    Added dummy constants file.

---
+++ src/assets/constants.ts
@@ -0,0 +1,19 @@
+const navMenu = [
+  {
+    id: 1,
+    title: "Learn more",
+    route: "/learn-more",
+  },
+  {
+    id: 2,
+    title: "List your car",
+    route: "/list-you-car",
+  },
+  {
+    id: 3,
+    title: "Inbox",
+    route: "/inbox",
+  },
+];
+
+export { navMenu };
