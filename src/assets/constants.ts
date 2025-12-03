66ca7c826ecca1d64eab74465b03b9a0c95fcfca
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-03 19:11:44 +0530

    Added dummy step form data.

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -16,4 +16,73 @@ const navMenu = [
   },
 ];
 
-export { navMenu };
+const stepperForm = [
+  {
+    id: 1,
+    title: "Location",
+    route: "/location",
+    status: "completed",
+  },
+  {
+    id: 2,
+    title: "About",
+    route: "/about",
+    status: "completed",
+  },
+  {
+    id: 3,
+    title: "Features",
+    route: "/features",
+    status: "completed",
+  },
+  {
+    id: 4,
+    title: "Rules",
+    route: "/rules",
+    status: "completed",
+  },
+  {
+    id: 5,
+    title: "Pricing",
+    route: "/pricing",
+    status: "completed",
+  },
+  {
+    id: 6,
+    title: "Promotion",
+    route: "/promotion",
+    status: "completed",
+  },
+  {
+    id: 7,
+    title: "Pictures",
+    route: "/pictures",
+    status: "completed",
+  },
+  {
+    id: 8,
+    title: "Insurance",
+    route: "/insurance",
+    status: "completed",
+  },
+  {
+    id: 9,
+    title: "Subscription",
+    route: "/subscription",
+    status: "active",
+  },
+  {
+    id: 10,
+    title: "Device",
+    route: "/device",
+    status: "pending",
+  },
+  {
+    id: 11,
+    title: "Easy Access",
+    route: "/easy-access",
+    status: "pending",
+  },
+];
+
+export { navMenu, stepperForm };
