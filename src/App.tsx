aed59335010cb100138cac1bce007d716b8c0c61
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-03 22:31:57 +0530

    added main page css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/layout/header/header";
 import StepperForm from "./components/layout/sidebar/stepperForm";
+import Subscription from "./components/subscription/Subscription";
 
 function App() {
   return (
     <>
       <Header />
-      <StepperForm />
+      <div className="main-page">
+        <StepperForm />
+        <Subscription />
+      </div>
     </>
   );
 }
