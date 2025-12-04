3f8c98a9ffd2067040c6922606c3a12061db757b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-04 21:24:07 +0530

    Added device management page.

---
+++ src/components/device-management/device-management.tsx
@@ -0,0 +1,24 @@
+import AddDevice from "./add-device";
+import "./device-management.scss";
+
+const DeviceManagement = () => {
+  return (
+    <section className="wrapper">
+      <section>
+        <h1>Device management</h1>
+        <p className="subtitle">
+          Add details of the device, if any already installed on your car. If
+          none, then continue to next step.
+        </p>
+      </section>
+
+      <hr className="separator" />
+      <AddDevice deviceId={1} />
+      <AddDevice deviceId={2} />
+      <AddDevice deviceId={3} />
+      <AddDevice deviceId={4} />
+    </section>
+  );
+};
+
+export default DeviceManagement;
