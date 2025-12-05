32a54b19455c217ad6aa7f7a5d089d685fb21080
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-05 19:46:50 +0530

    connected store with nextlistitem button.

--- src/components/listing/NextListItemButton.tsx
+++ src/components/listing/NextListItemButton.tsx
@@ -1,10 +1,25 @@
+import { useListingStore } from "@/store/listing.store";
+import { stepperForm } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
 import "./NextListItemButton.scss";
 
 const NextListItemButton = () => {
+  const { selectedPlan, currentStep, goToStep } = useListingStore();
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    if (currentStep < stepperForm.length) {
+      goToStep(currentStep + 1);
+      navigate(`/listing${stepperForm[currentStep].route}`);
+    }
+  };
+
   return (
     <section id="bottom-navigation-wrapper">
       <section id="bottom-navigation">
-        <button>Next</button>
+        <button onClick={handleNext} disabled={selectedPlan === ""}>
+          Next
+        </button>
       </section>
     </section>
   );
