620b4025eebeb9c86ad03a8a5bc839d111835498
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-12-07 15:15:19 +0530

    Installed testing libraries + configured vite.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
